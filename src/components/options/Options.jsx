a3ac3d6c1561a7a59138dee6ca07c95946987723
Juan Ignacio Errecart <jierrecart5@gmail.com>
2026-06-22 18:56:30 -0300

    [new]: update de CV, [chnages]: sidebar update

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -32,8 +32,6 @@ const Options = () => {
     }
   };
 
-  
-
   const handleClick = (section) => {
     setActiveSection(section);
     const element = document.getElementById(section);
@@ -50,14 +48,31 @@ const Options = () => {
   return (
     <div className="options_container">
       <div className="options">
-        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+        {/* <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M4 0H6V2H10V4H8.86807C8.57073 5.66996 7.78574 7.17117 6.6656 8.35112C7.46567 8.73941 8.35737 8.96842 9.29948 8.99697L10.2735 6H12.7265L15.9765 16H13.8735L13.2235 14H9.77647L9.12647 16H7.0235L8.66176 10.9592C7.32639 10.8285 6.08165 10.3888 4.99999 9.71246C3.69496 10.5284 2.15255 11 0.5 11H0V9H0.5C1.5161 9 2.47775 8.76685 3.33437 8.35112C2.68381 7.66582 2.14629 6.87215 1.75171 6H4.02179C4.30023 6.43491 4.62904 6.83446 4.99999 7.19044C5.88743 6.33881 6.53369 5.23777 6.82607 4H0V2H4V0ZM12.5735 12L11.5 8.69688L10.4265 12H12.5735Z"
 
           />
-        </svg>
+        </svg> */}
+        <a
+          href="CV/JuanIgnacioErrecartCV.pdf"
+          download="Juan Ignacio Errecart CV"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="200"
+            height="200"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              fill="currentColor"
+              d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11l-5 5Zm-6 4q-.825 0-1.413-.588T4 18v-3h2v3h12v-3h2v3q0 .825-.588 1.413T18 20H6Z"
+            />
+          </svg>
+        </a>
 
         {isDarkMode ? (
           <svg
